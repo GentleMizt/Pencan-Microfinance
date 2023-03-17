4df9081faa7effc883c32736a32f2a0d6c716a07
GentleMizt <olamideiyanda18@gmail.com>
2023-03-17 16:02:08 +0100

    progress

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,19 @@
-    window.addEventListener("load", function() {
-        let loader = document.getElementById('preloader');
-        console.log('load event triggered');
-        loader.style.display = 'none';
-    });
+'use strict';
+  AOS.init();
+  window.addEventListener('load', function () {
+    let loader = document.getElementById('preloader');
+    console.log('load event triggered');
+    loader.style.display = 'none';
+  });
 
-    let bannerImages = [
-        './dist/Cimg/banner1.png',
-        './dist/Cimg/scnd-main-img.png'
-      ];
-      
-      let currentImageIndex = 0;
-      let bannerImage = document.querySelector('.banner img');
-      
-      function changeBannerImage() {
-        currentImageIndex = (currentImageIndex + 1) % bannerImages.length;
-        bannerImage.src = bannerImages[currentImageIndex];
-      }
-      
-      setInterval(changeBannerImage, 2500);
+  let bannerImages = ['./dist/Cimg/banner1.png', './dist/Cimg/scnd-main-img.png'];
+
+  let currentImageIndex = 0;
+  let bannerImage = document.querySelector('.banner img');
+
+  function changeBannerImage() {
+    currentImageIndex = (currentImageIndex + 1) % bannerImages.length;
+    bannerImage.src = bannerImages[currentImageIndex];
+  }
+
+  setInterval(changeBannerImage, 2500);
