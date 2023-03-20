7c2e1d6ca0a61fd741aca93e043450b4b4bfc5af
GentleMizt <olamideiyanda18@gmail.com>
2023-03-20 13:39:55 +0100

    progress

--- dist/faq.js
+++ dist/faq.js
@@ -2,17 +2,31 @@
 
 const savingClick = document.querySelector('.saving');
 const loanClick = document.querySelector('.loan');
+// const plusOpen = document.querySelector('.plus-open');
+const opener = document.querySelector('.opener');
+let plusOpenSrc = './dist/Cimg/open-plus.png';
+let closeTimesSrc = './dist/Cimg/close-times.png';
 
-console.log(loanClick);
+loanClick.addEventListener('click', e => {
+  loanClick.style.backgroundColor = 'rgba(217, 217, 217, 0.9)';
+  loanClick.style.borderRadius = '1rem';
+  savingClick.style.backgroundColor = '#1C9FF3';
+});
 
-loanClick.addEventListener('click', (e) =>{
-    loanClick.style.backgroundColor = 'rgba(217, 217, 217, 0.9)';
-    loanClick.style.borderRadius = '1rem';
-    savingClick.style.backgroundColor = '#1C9FF3'
-  })
-  
-  savingClick.addEventListener('click', () =>{
-    savingClick.style.backgroundColor = 'rgba(217, 217, 217, 0.9)';
-    savingClick.style.borderRadius = '1rem';
-    loanClick.style.backgroundColor = '#1C9FF3'
-  })
+savingClick.addEventListener('click', () => {
+  savingClick.style.backgroundColor = 'rgba(217, 217, 217, 0.9)';
+  savingClick.style.borderRadius = '1rem';
+  loanClick.style.backgroundColor = '#1C9FF3';
+});
+
+opener.addEventListener('click', (e) => {
+    // console.log(opener.src);
+
+  if (opener.src === plusOpenSrc) {
+    opener.src = './dist/Cimg/close-times.png';
+  } else {
+    opener.src = './dist/Cimg/open-plus.png';
+  }
+});
+
+const toggleOpener = () => {};
